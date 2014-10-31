3d3e08030d3e9eef156b10ef92d29a8017fada8d
Kane Baccigalupi <baccigalupi@gmail.com>
2014-10-31 14:53:28 -0700

    Feedback
    Looks good. Small nit about rand for the age

--- ageist_a.rb
+++ ageist_a.rb
@@ -1,6 +1,8 @@
-age = rand(110)
+age = rand(110) # check the docs for rand, current usage will never go to the top of the range
 puts "Age is #{age}"
 
+# love the use of between, but do you need to check the lower limit after the first branch of if?
+# Can't you get away with just using a <= test?
 if age.between?(0,1)
   puts "baby"
 elsif age.between?(2,10)
